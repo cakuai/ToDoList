ae3b25673e81111d59dbe536dd01217e000fef62
cakuai <angel09931935@gmail.com>
2023-09-27 01:59:29 +0800

    Text"
    clear
    e

--- test.js
+++ test.js
@@ -7,30 +7,27 @@ while (input !== "quit") {
     if (input == "new") {
         let new_item = prompt("What's the new task?");
         TodoList.push(new_item);
-
         console.log(`${new_item} added.`);
-        input = prompt("Please Enter an Instruction");
     }
 
     else if (input == "list") {
         for (let i of TodoList) {
             console.log(`${TodoList.indexOf(i)} : ${i}`);
         }
-
-        input = prompt("Please Enter an Instruction");
     }
 
     else if (input == "del") {
         let del_item = prompt("Which one would you like to delete?");
         TodoList.splice(del_item, 1);
-
-        input = prompt("Please Enter an Instruction");
     }
 
     else {
         input = prompt("Please enter a Valid Instruction.");
     }
 
+    input = prompt("Please Enter an Instruction");
+
 }
 
 console.log("END");
+console.log("test");
